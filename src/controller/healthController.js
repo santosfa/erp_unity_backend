88671f9938c6a1a38afda9edbbdbddb6c9a62bba
Fabiano dos Santos <Fabiano.dos.stos@gmail.com>
2024-05-02 23:01:06 -0300

    Alterando o health status

--- src/controller/healthController.js
+++ src/controller/healthController.js
@@ -2,7 +2,7 @@
 class healthController {
     status(req, res){
         return res.status(200).json(
-            {"message":"Service on line."}
+            {"message":"Servico esta fucionando."}
         );
     };
 };
